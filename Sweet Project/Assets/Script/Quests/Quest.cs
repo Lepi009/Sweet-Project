a5cb7b4f6429243fc588424d3e51aff88a03d055
Lepi009 <leander.pieters@web.de>
2020-03-30 11:34:57 +0200

    first coding issues

--- Sweet Project/Assets/Script/Quests/Quest.cs
+++ Sweet Project/Assets/Script/Quests/Quest.cs
@@ -2,12 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Quest
+namespace Com.LepiStudios.myQuestSystem
 {
-    public QuestList questName;
-    public abstract void Fire();
+    /// <summary>
+    /// this class is the abstract parent class for all types of quest and defines the most common and needed methods
+    /// </summary>
+    public abstract class Quest
+    {
+        #region Attributes
 
-    public abstract void Update();
+        /// <summary>
+        /// every quest should have a quest name which is saved in the QuestList enum
+        /// </summary>
+        public QuestList questName;
 
-    public abstract void Finish();
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// the fire methods starts the quest and initalize the quest, e.g. spawning items, show something in a UI
+        /// </summary>
+        public abstract void Fire();
+
+        /// <summary>
+        /// the update method is called from outside and updates some values, e.g. count something up
+        /// </summary>
+        public abstract void Update();
+
+        /// <summary>
+        /// the finish method is called to finish the quest, e.g. start next quest or brake some barricades
+        /// </summary>
+        public abstract void Finish();
+
+        #endregion
+    }
+
 }
--- Sweet Project/Assets/Script/Quests/Quest_Collect.cs
+++ Sweet Project/Assets/Script/Quests/Quest_Collect.cs
@@ -2,36 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Quest_Collect : Quest
-{
-    public override void Fire()
-    {
-        //updates all barricades
-        QuestBarricade[] barricades = Object.FindObjectsOfType<QuestBarricade>();
-        foreach(QuestBarricade barricade in barricades)
-        {
-            barricade.Proof(TimeOfQuest.Beginning, questName);
-        }
 
-        //updates all items
-        QuestItem[] questItems = Object.FindObjectsOfType<QuestItem>();
-        foreach (QuestItem item in questItems)
+namespace Com.LepiStudios.myQuestSystem { 
+
+    /// <summary>
+    /// this abstract class is subclass 
+    /// </summary>
+    public abstract class Quest_Collect : Quest
+    {
+        public override void Fire()
         {
-            if (item.quest == questName)
+            //updates all barricades
+            QuestBarricade[] barricades = Object.FindObjectsOfType<QuestBarricade>();
+            foreach(QuestBarricade barricade in barricades)
             {
-                item.ShowItem();
+                barricade.Proof(TimeOfQuest.Beginning, questName);
+            }
+
+            //updates all items
+            QuestItem[] questItems = Object.FindObjectsOfType<QuestItem>();
+            foreach (QuestItem item in questItems)
+            {
+                if (item.quest == questName)
+                {
+                    item.ShowItem();
+                }
             }
         }
-    }
 
-    public override void Finish()
-    {
-        //updates all barricades
-        QuestBarricade[] barricades = Object.FindObjectsOfType<QuestBarricade>();
-        foreach (QuestBarricade barricade in barricades)
+        public override void Finish()
         {
-            barricade.Proof(TimeOfQuest.Ending, questName);
-        }
+            //updates all barricades
+            QuestBarricade[] barricades = Object.FindObjectsOfType<QuestBarricade>();
+            foreach (QuestBarricade barricade in barricades)
+            {
+                barricade.Proof(TimeOfQuest.Ending, questName);
+            }
 
+        }
     }
 }
