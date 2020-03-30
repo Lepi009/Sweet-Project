e4e7c3dece7bbbd0806534bcc8ba279b91d12f6b
Lepi009 <60300421+Lepi009@users.noreply.github.com>
2020-03-30 12:14:17 +0200

    beauty update

--- Sweet Project/Assets/Script/Quests/Quest_CollectPPAP.cs
+++ Sweet Project/Assets/Script/Quests/Quest_CollectPPAP.cs
@@ -5,36 +5,37 @@ using UnityEngine.UI;
 
 public class Quest_CollectPPAP : Quest_Collect
 {
-    private GameObject banana;
-
-    private GameObject player;
-
+    ///<summary>var safes the "quest board", which shows the current running quest</summary>
     private Text questText;
-
+    
+    ///<summary>var counts the founded items</summary>
     public int foundItems = 0;
 
     public override void Fire()
     {
         questName = QuestList.CollectPPAP;
+        
         base.Fire();
+        
         questText = GameObject.FindGameObjectWithTag("QuestText").GetComponent<Text>();
 
     }
 
     public override void Update()
     {
-        if(foundItems == 1)
+        //method called whenever a fruit is found
+        foundItems++;
+        
+        if(foundItems == 2) //called when the second fruit was found
         {
             Finish();
-        } else
-        {
-            foundItems++;
         }
     }
 
     public override void Finish()
     {
         base.Finish();
+        
         questText.text = "You finished the PPAP quest...now sing for me\nAnd maybe there is now a way free that was not free yet";
     }
 
