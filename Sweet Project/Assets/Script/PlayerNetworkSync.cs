ede856b97968e5c8f94f352788c37bd795653b7b
Lepi009 <60300421+Lepi009@users.noreply.github.com>
2020-03-30 14:21:31 +0200

    beauty update

--- Sweet Project/Assets/Script/PlayerNetworkSync.cs
+++ Sweet Project/Assets/Script/PlayerNetworkSync.cs
@@ -1,43 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Com.LepiStudios.SweetProject;
 
 using Photon.Pun;
 using Photon.Realtime;
 using ExitGames.Client.Photon;
 
-public class PlayerNetworkSync : MonoBehaviourPunCallbacks, IPunObservable
+namespace Com.LepiStudios.SweetProject
 {
-    private Rigidbody2D rb;
 
-    private Animator animator;
-
-    private SpriteRenderer spriteRenderer;
-
-    private PlayerController playerController;
-
-    private Vector2 previousVelocity = Vector2.zero;
-
-    private bool jumpTrigger = false;
-
-    private void Start()
+    ///<summary>The scripts should be the component of the player, it syncs the player through the internet, should be unique for every project</summary>
+    public class PlayerNetworkSync : MonoBehaviourPunCallbacks, IPunObservable
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        playerController = GetComponent<PlayerController>();
+        #region Private Fields
 
-        if (!photonView.IsMine) rb.gravityScale = 0;
-        else
+        ///<summary>The rigidbody of the player</summary>
+        private Rigidbody2D rb;
+
+        ///<summary>The animator of the player</summary>
+        private Animator animator;
+
+        ///<summary>The sprite renderer of the player</summary>
+        private SpriteRenderer spriteRenderer;
+
+        ///<summary>The player controller of the player</summary>
+        private PlayerController playerController;
+
+        ///<summary>var safes the previous velocity of the player, in case that the player does not have authentication on this game object</summary>
+        private Vector2 previousVelocity = Vector2.zero;
+
+        ///<summary>var safes if the animator's jump trigger was pressed last frame</summary>
+        private bool jumpTrigger = false;
+
+        #endregion
+
+        private void Start()
         {
-            //StartCoroutine(UpdateNetworkFixed());
-        }
-    }
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            playerController = GetComponent<PlayerController>();
 
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if(stream.IsWriting)
+            if (!photonView.IsMine) rb.gravityScale = 0; //otherwise there are some weird behaviours because the synced velocity + in-game gravity is false
+        }
+
+        #region Pun Callbacks
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+        if(stream.IsWriting) //upload all important information
         {
             stream.SendNext(rb.position);
             stream.SendNext(rb.velocity);
@@ -45,15 +56,18 @@ public class PlayerNetworkSync : MonoBehaviourPunCallbacks, IPunObservable
             stream.SendNext(animator.GetBool("Fall"));
             stream.SendNext(jumpTrigger);
             jumpTrigger = false;
-        } else
+        } 
+        else //download all important information
         {
             //update position
             Vector2 newPosition = (Vector2) stream.ReceiveNext();
             Vector2 velocity = (Vector2) stream.ReceiveNext();
             rb.velocity = velocity;
+            //calculates the lag, the difference is the delay between the sending and the receiving
             float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
             rb.position = newPosition + velocity * lag;
-
+            
+            //update rotation
             if (velocity.x < 0) spriteRenderer.flipX = true;
             else if (velocity.x > 0) spriteRenderer.flipX = false;
 
@@ -66,55 +80,19 @@ public class PlayerNetworkSync : MonoBehaviourPunCallbacks, IPunObservable
             if(jump) animator.SetTrigger("Jump");
         }
     }
-
+    
+    #endregion
+    
+    #region Public Methods
+    
+    ///<summary>Gets called by the controller script to safe if the jump trigger was triggered</summary>
     public void JumpTriggerUpdate()
     {
         jumpTrigger = true;
     }
+    
+    #endregion
 
-    /*IEnumerator UpdateNetworkFixed()
-    {
-        while(true)
-        {
-            object[] data = new object[]
-            {
-
-            };
-            RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All, CachingOption = EventCaching.DoNotCache };
-            SendOptions sendOptions = new SendOptions { Reliability = true };
-            PhotonNetwork.RaiseEvent(3, false, raiseEventOptions, sendOptions);
-
-            photonView.RPC(nameof(RPC_RecieveNewPosition), RpcTarget.OthersBuffered, rb.position, rb.velocity);
-            photonView.RPC(nameof(RPC_RecieveAnimator), RpcTarget.OthersBuffered, animator.GetBool("Walking"), animator.GetBool("Fall"));
-            yield return new WaitForSeconds(0.025f);
-        }
     }
-
-     [PunRPC]
-     public void RPC_RecieveNewPosition(Vector2 newPosition, Vector2 velocity, PhotonMessageInfo info)
-     {
-        
-        rb.velocity = velocity;
-        float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
-        rb.position = newPosition + velocity * lag;
-
-
-        if (velocity.x < 0) spriteRenderer.flipX = true;
-        else if(velocity.x > 0) spriteRenderer.flipX = false;
-    }
-
-    [PunRPC]
-    public void RPC_RecieveAnimator(bool walking, bool fall)
-    {
-        if (animator.GetBool("Walking") != walking) animator.SetBool("Walking", walking);
-        if (animator.GetBool("Fall") != fall) animator.SetBool("Fall", fall);
-    }
-
-
-    [PunRPC]
-    public void RPC_JumpTriggerUpdate()
-    {
-        animator.SetTrigger("Jump");
-    }*/
 
 }
